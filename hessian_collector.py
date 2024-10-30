fix hessian

hessian_collector.py
@@ -1,16 +1,19 @@
+# from https://github.com/Cornell-RelaxML/quip-sharp/blob/main/quantize_llama/hessian_offline_llama.py
+
 # from lib import utils
 import argparse
 import datetime
 import os
 import random
-from copy import deepcopy
 
 import numpy
 import torch
 import torch.multiprocessing as mp
-from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizerFast
-from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.modeling_attn_mask_utils import (
+    _prepare_4d_causal_attention_mask)
+
+from dataset import sample_rp1t
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
@@ -19,7 +22,8 @@ parser.add_argument('--seed', default=0, type=int)
 parser.add_argument('--batch_size', default=2, type=int)
 parser.add_argument('--devset_size', default=256, type=int)
 parser.add_argument('--ctx_size', default=4096, type=int)
-parser.add_argument('--base_model', default='meta-llama/Llama-2-70b-hf', type=str)
+parser.add_argument(
+    '--base_model', default='meta-llama/Llama-2-70b-hf', type=str)
 parser.add_argument('--save_path', default='hessians/llama2_70b', type=str)
 parser.add_argument('--scratch_path', default=None, type=str)
 parser.add_argument('--chunk_size', default=256, type=int)
@@ -44,15 +48,52 @@ def move_fn(in_q, async_copy_speed):
         print(f'moved {src} to {tgt}')
 
 
+def register_H_hook(module, device):
+    n = module.in_features
+    H = torch.zeros(n, n, dtype=torch.float64, device=device)
+    mu = torch.zeros(n, dtype=torch.float64, device=device)
+    ct = 0
+
+    def H_hook(module, x):
+        nonlocal H, mu, ct, n
+        x = x[0].reshape(-1, n).to(torch.float64)
+        mu.add_(x.sum(dim=0))
+        H.addmm_(x.T, x)
+        ct += len(x)
+
+    hook = module.register_forward_pre_hook(H_hook)
+
+    def done():
+        nonlocal H, mu, ct, hook
+        hook.remove()
+        return H.cpu(), mu.cpu(), ct
+
+    return done
+
+
+def flat_to_sym(V, N):
+    A = torch.zeros(N, N, dtype=V.dtype, device=V.device)
+    idxs = torch.tril_indices(N, N, device=V.device)
+    A[idxs.unbind()] = V
+    A[idxs[1, :], idxs[0, :]] = V
+    return A
+
+
+def sym_to_flat(A):
+    N = A.shape[-1]
+    idxs = torch.tril_indices(N, N, device=A.device)
+    return A[idxs.unbind()]
+
+
 def forward_layer(layer, position_ids, attention_mask, bs, device, in_q, out_q):
     torch.set_grad_enabled(False)
     layer = layer.to(device)
     position_ids = position_ids.to(device)
     attention_mask = attention_mask.to(device)
-    done_qkv = utils.register_H_hook(layer.self_attn.q_proj, device)
-    done_o = utils.register_H_hook(layer.self_attn.o_proj, device)
-    done_up = utils.register_H_hook(layer.mlp.up_proj, device)
-    done_down = utils.register_H_hook(layer.mlp.down_proj, device)
+    done_qkv = register_H_hook(layer.self_attn.q_proj, device)
+    done_o = register_H_hook(layer.self_attn.o_proj, device)
+    done_up = register_H_hook(layer.mlp.up_proj, device)
+    done_down = register_H_hook(layer.mlp.down_proj, device)
 
     while True:
         dev_emb = in_q.get()
@@ -60,7 +101,8 @@ def forward_layer(layer, position_ids, attention_mask, bs, device, in_q, out_q):
             layer = layer.cpu()
             position_ids = position_ids.cpu()
             attention_mask = attention_mask.cpu()
-            out_q.put({'qkv': done_qkv(), 'o': done_o(), 'up': done_up(), 'down': done_down()})
+            out_q.put({'qkv': done_qkv(), 'o': done_o(),
+                      'up': done_up(), 'down': done_down()})
             return
 
         assert len(dev_emb) % bs == 0
@@ -83,15 +125,17 @@ def accumulate(in_q, move_q, ngpus, args, transformer_layer_index):
         out = in_q.get()
         if i == 0:
             for key in out:
-                Hs[key] = torch.zeros(out[key][0].shape, dtype=out[key][0].dtype)
-                mus[key] = torch.zeros(out[key][1].shape, dtype=out[key][1].dtype)
+                Hs[key] = torch.zeros(
+                    out[key][0].shape, dtype=out[key][0].dtype)
+                mus[key] = torch.zeros(
+                    out[key][1].shape, dtype=out[key][1].dtype)
                 cts[key] = 0
         for key in out:
             Hs[key].add_(out[key][0])
             mus[key].add_(out[key][1])
             cts[key] += out[key][2]
 
-    keys = list(Hs.keys())
+    # keys = list(Hs.keys())
 
     for key in Hs:
         mus[key].div_(cts[key])
@@ -99,7 +143,7 @@ def accumulate(in_q, move_q, ngpus, args, transformer_layer_index):
         Hs[key].addmm_(-mus[key].unsqueeze(-1), mus[key].unsqueeze(0))
         save_path = f"{args.scratch_path}/{transformer_layer_index}_{key}.pt" if args.scratch_path is not None else f"{args.save_path}/{transformer_layer_index}_{key}.pt"
         torch.save({
-            'flatH': utils.sym_to_flat(Hs[key].to(torch.float32)),
+            'flatH': sym_to_flat(Hs[key].to(torch.float32)),
             'mu': mus[key].to(torch.float32),
             'n': Hs[key].shape[0],
             'ct': cts[key]
@@ -127,13 +171,16 @@ def main(args):
 
     if os.path.isfile(f"{args.save_path}/dev_activations.pt"):
         print("loading cached dataset...")
-        loaded_dev_activations = torch.load(f"{args.save_path}/dev_activations.pt")
+        loaded_dev_activations = torch.load(
+            f"{args.save_path}/dev_activations.pt")
         after_layer = loaded_dev_activations['after_layer']
         dev_emb = loaded_dev_activations['dev_emb']
-        print(f"loaded cached dataset from {loaded_dev_activations['timestamp']}")
+        print(
+            f"loaded cached dataset from {loaded_dev_activations['timestamp']}")
     else:
         print("loading dataset...")
-        devset = utils.sample_rp1t(tokenizer, args.devset_size, args.ctx_size, nproc=args.sample_proc)
+        devset = sample_rp1t(
+            tokenizer, args.devset_size, args.ctx_size, nproc=args.sample_proc)
         dev_emb = model.model.embed_tokens(devset)
         after_layer = -1
         print("loaded dataset!")
@@ -152,12 +199,14 @@ def main(args):
         )
     else:
         attention_mask = _prepare_4d_causal_attention_mask(
-            None, (args.batch_size, args.ctx_size), dev_emb[0:args.batch_size], 0
+            None, (args.batch_size,
+                   args.ctx_size), dev_emb[0:args.batch_size], 0
         )
 
     if args.scratch_path is not None:
         move_q = mp.Queue()
-        move_p = mp.Process(target=move_fn, args=(move_q, args.async_copy_speed))
+        move_p = mp.Process(target=move_fn, args=(
+            move_q, args.async_copy_speed))
         move_p.start()
     else:
         move_q = None
@@ -171,7 +220,8 @@ def main(args):
 
         transformer_layer = model.model.layers[transformer_layer_index]
         # check that there are four layers, as expected
-        assert (len([m for m in transformer_layer.modules() if isinstance(m, torch.nn.Linear)]) == 7)
+        assert (len([m for m in transformer_layer.modules()
+                if isinstance(m, torch.nn.Linear)]) == 7)
 
         chunk_size = min(args.chunk_size, len(dev_emb))
         ngpus = min(torch.cuda.device_count(), len(dev_emb) // chunk_size)
@@ -180,19 +230,22 @@ def main(args):
         in_q = manager.Queue()
         out_q = manager.Queue()
 
-        accumulate_proc = mp.Process(target=accumulate, args=(out_q, move_q, ngpus, args, transformer_layer_index))
+        accumulate_proc = mp.Process(target=accumulate, args=(
+            out_q, move_q, ngpus, args, transformer_layer_index))
         accumulate_proc.start()
 
         forward_procs = []
         for i in range(ngpus):
             p = mp.Process(
                 target=forward_layer,
-                args=(transformer_layer, position_ids, attention_mask, args.batch_size, i, in_q, out_q)
+                args=(transformer_layer, position_ids,
+                      attention_mask, args.batch_size, i, in_q, out_q)
             )
             p.start()
             forward_procs.append(p)
 
-        assert len(dev_emb) % args.batch_size == 0 and chunk_size % args.batch_size == 0
+        assert len(
+            dev_emb) % args.batch_size == 0 and chunk_size % args.batch_size == 0
         i = 0
         while i < len(dev_emb):
             next = min(i + chunk_size, len(dev_emb))
@@ -212,7 +265,8 @@ def main(args):
         utils.clean()
 
         if args.save_activations and (
-            transformer_layer_index % args.act_save_rate == 0 or transformer_layer_index == len(model.model.layers) - 1
+            transformer_layer_index % args.act_save_rate == 0 or transformer_layer_index == len(
+                model.model.layers) - 1
         ):
             if args.scratch_path is not None:
                 if os.path.exists(f'{args.scratch_path}/dev_activations.pt'):
@@ -223,7 +277,8 @@ def main(args):
                         'after_layer': transformer_layer_index,
                         'timestamp': str(datetime.datetime.now())
                     }, f'{args.scratch_path}/dev_activations.pt')
-                    move_q.put((f'{args.scratch_path}/dev_activations.pt', f'{args.save_path}/dev_activations.pt'))
+                    move_q.put((f'{args.scratch_path}/dev_activations.pt',
+                               f'{args.save_path}/dev_activations.pt'))
             else:
                 torch.save({
                     'dev_emb': dev_emb,