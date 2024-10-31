fix hessian code

hessian_collector.py
@@ -89,7 +89,6 @@ def sym_to_flat(A):
 def forward_layer(layer, position_ids, attention_mask, bs, device, in_q, out_q):
     torch.set_grad_enabled(False)
     layer = layer.to(device)
-    layer.cpu_layer = None
     position_ids = position_ids.to(device)
     attention_mask = attention_mask.to(device)
 
@@ -174,7 +173,10 @@ def clean():
 def main(args):
     print("loading model...")
     model = AutoModelForCausalLM.from_pretrained(
-        args.base_model, torch_dtype="auto", low_cpu_mem_usage=True, trust_remote_code=True, device_map='auto'
+        args.base_model,
+        torch_dtype="auto",
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
     )
     print("loaded model!")
     tokenizer = AutoTokenizer.from_pretrained(args.base_model, use_fast=True)
@@ -198,6 +200,7 @@ def main(args):
 
     position_ids = torch.arange(args.ctx_size, dtype=torch.int64)[None, :] + \
         torch.zeros(args.batch_size, args.ctx_size, dtype=torch.int64)
+
     if hasattr(model.config, 'sliding_window'):
         attention_mask = _prepare_4d_causal_attention_mask(
             None, (args.batch_size, args.ctx_size),
@@ -225,10 +228,6 @@ def main(args):
             continue
 
         transformer_layer = model.model.layers[transformer_layer_index]
-        if transformer_layer_index > 0:
-            del model.model.layers[transformer_layer_index - 1]
-            torch.cuda.empty_cache()
-
         # check that there are four layers, as expected
         assert (len([m for m in transformer_layer.modules() if isinstance(m, torch.nn.Linear)]) == 7)
 