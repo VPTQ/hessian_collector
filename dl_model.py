7d7070446e6096e8985c977993516e285cf19a4f
Yang Wang <wyatuestc@gmail.com>
2024-11-02 09:12:19 -0700

    try llama3.2

---
+++ dl_model.py
@@ -0,0 +1,4 @@
+# pip install hf_transfer
+HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download 'meta-llama/Llama-3.2-11B-Vision-Instruct' 
+
+HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download 'meta-llama/Llama-3.2-90B-Vision-Instruct' 
