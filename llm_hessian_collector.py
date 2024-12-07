fix llm hessian

llm_hessian_collector.py
@@ -12,13 +12,18 @@ import psutil
 import torch
 import torch.cuda.streams
 import torch.multiprocessing as mp
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+# from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
 import torch
 from PIL import Image
+# from transformers import MllamaForConditionalGeneration, AutoProcessor
+from transformers import AutoTokenizer, AutoProcessor, image_utils
+from transformers import AutoModelForCausalLM
 
 from accelerate import Accelerator
 from accelerate import dispatch_model
+
+from cli_datasets import sample_rp1t
 
 import pickle
 
@@ -26,17 +31,12 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', default=0, type=int)
-parser.add_argument('--base_model', default='Qwen/Qwen2.5-Coder-32B-Instruct', type=str)
-parser.add_argument('--save_path', default='Hessians-Qwen25-Coder-32B-Instruct', type=str)
-parser.add_argument('--sample_proc', default=4, type=int)
+parser.add_argument('--base_model', default='Qwen/Qwen2-57B-A14B-Instruct', type=str)
+parser.add_argument('--save_path', default='Hessians-Qwen2-57B-A14B-Instruct', type=str)
+parser.add_argument('--sample_proc', default=72, type=int)
 parser.add_argument('--save_mem', default=False, type=bool)
-parser.add_argument('--text_dir', default='/home/aiscuser/yangwang/data_sample/', type=str)
-parser.add_argument('--max_samples', default=10000, type=int)
-parser.add_argument('--load_file_list', default=None, type=str)
-parser.add_argument('--store_file_list', default=None, type=str)
-parser.add_argument('--num_part', default=1, type=int)
-parser.add_argument('--part_id', default=0, type=int)
-
+parser.add_argument('--max_samples', default=2000, type=int)
+parser.add_argument('--ctx_size', default=8192, type=int)
 def move_fn(in_q, async_copy_speed):
     # async copy to avoid slow disk
     while True:
@@ -109,6 +109,7 @@ def sym_to_flat(A):
     idxs = torch.tril_indices(N, N, device=A.device)
     return A[idxs.unbind()]
 
+
 def clean():
     gc.collect()
     torch.cuda.empty_cache()
@@ -128,15 +129,24 @@ def main(args):
      
     if accelerator.is_main_process:
         print(f"Total GPUs available: {torch.cuda.device_count()}")
+    
+    print(f"args.base_model: {args.base_model}")
+    print(f"loading dataset...")
 
-    print("loading model...")
     model = AutoModelForCausalLM.from_pretrained(
         args.base_model,
         torch_dtype="auto",
-        low_cpu_mem_usage=True,
-        trust_remote_code=True,
+        device_map="auto"
     )
+    
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
 
+    devset = sample_rp1t(tokenizer, args.max_samples, args.ctx_size, nproc=args.sample_proc)
+    dev_emb = model.model.embed_tokens(devset)
+    print(f"loaded dataset! {dev_emb.shape}")
+
+    print("loading model...")
+    
     model.tie_weights()
     # device_map = dispatch_model(model, device_map="auto")
     # transformer_layers = distribute_layers_across_gpus(model, num_gpus)
@@ -144,87 +154,25 @@ def main(args):
     model = model.eval()
     model = accelerator.prepare(model)
     hooks = []
-    # HACK: split odd/even layers
     for layer_idx, layer in enumerate(model.named_modules()):
         print(f'layer_idx: {layer_idx}, layer: {layer}')
-        # language_model.model.layers.0.mlp.down_proj.pt
-        parts = layer[0].split('.')
-        print(parts)
-        try:
-            # Try to parse the layer index from parts that typically contain numbers
-            model_layer_idx = int(parts[3])
-        except (IndexError, ValueError):
-            model_layer_idx = 0
-            continue
-        
-        if isinstance(layer[1], torch.nn.Linear) and model_layer_idx % args.num_part == args.part_id:
-        # if isinstance(layer[1], torch.nn.Linear) and 'language_model.model' in layer[0]:
+        if isinstance(layer[1], torch.nn.Linear):
             device = next(layer[1].parameters()).device
             hook = register_H_hook(layer[1], device, args.save_mem)
             hooks.append((f"{layer[0]}", hook))
     
     print(f"hooks: {hooks}")
+
     
-    if args.load_file_list is not None:
-        with open(args.load_file_list, 'rb') as f:
-            text_files = pickle.load(f)
-        print(f'load file list to {args.load_file_list}')
-    else: 
-        text_files = sorted([f for f in os.listdir(args.text_dir) if f.endswith(('.json'))])
-        # shuffle
-        random.shuffle(text_files)
-        text_files = text_files[:args.max_samples]
-    
-        if args.store_file_list is not None:
-            with open(args.store_file_list, 'wb') as f:
-                pickle.dump(text_files, f)
-            print(f'store file list to {args.store_file_list}')
-    
-    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
-     
     idx = 0
-    import json
-    for text_file in text_files:
-        # Get corresponding text file name (assuming same base name, different extension)
-        base_name = os.path.splitext(text_file)[0]
-        text_file = os.path.join(args.text_dir, f"{base_name}.json")
-        
-        with open(text_file, "r") as f:
-            json.load(f)
-            # text = f.read()
-        text = json.load(f)['code']
-        # truncate input text
-        def _truncate_and_decode(text, tokenizer, max_length):
-            # Encode the text with truncation
-            encoded = tokenizer.encode(text)    
-            encoded = encoded[:max_length]
-            # Decode the truncated text
-            truncated_text = tokenizer.decode(encoded, skip_special_tokens=True)
-            return truncated_text
-
-        text = _truncate_and_decode(text, processor.tokenizer, 8192) 
-
+    for idx in range(args.max_samples):
+        input_ids = dev_emb[idx]
          
         device = accelerator.device
         
-        # print(f"input_text: {input_text}")
-        # print(f'len input_text: {len(input_text)}')
-         
-        try:
-            inputs = tokenizer(
-                text,
-                add_special_tokens=False,
-                return_tensors="pt",
-            ).to(device)
-            
-        except Exception as e:
-            print(f"Error processing {text_file}: {e}")
-            continue
-        
         with torch.no_grad():
-            outputs = model.generate(**inputs, max_new_tokens=30)
-            print(f"index: {idx}, processing {text_file}:")
-            # print(processor.decode(outputs[0]))
+            outputs = model.generate(input_ids, max_new_tokens=1)
+            print(f"index: {idx}, processing {input_ids.shape}:")
             print("-" * 50)
             
         idx += 1