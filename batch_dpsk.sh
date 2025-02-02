9cf28bcb4e78602438d3bb4761110e15e2693f22
Yang Wang <wyatuestc@gmail.com>
2025-02-02 09:42:27 -0800

    add bach deepseek script

---
+++ batch_dpsk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CKPT_PATH=~/yangwang/DeepSeek-V3-mp4/
+CONFIG_PATH=~/yangwang/DeepSeek-V3/inference/configs/config_671B.json
+
+for seed in {0..25}
+do
+    echo "Starting run with seed ${seed}"
+    
+    torchrun --nnodes 1 --nproc-per-node 4 deepseek_hessian_collector.py \
+        --ckpt_path ${CKPT_PATH} \
+        --config ${CONFIG_PATH} \
+        --save_path Hessians-Deepseek-V3-8192-400-seed-${seed} \
+        --max_samples 400 \
+        --ctx_size 8192 \
+        --sample_proc 16 \
+        --seed ${seed}
+        
+    echo "Completed run with seed ${seed}"
+    
+	sleep 10
+done
+
+echo "All runs completed!"
