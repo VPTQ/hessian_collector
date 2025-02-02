1c4a7e34cff3303e3c84f9ec89dd5f0f357d0f10
Yang Wang <wyatuestc@gmail.com>
2025-02-02 05:20:44 -0800

    add deepseek hessian

---
+++ deepseek_hessian_collector.py
@@ -0,0 +1,261 @@
+# from https://github.com/Cornell-RelaxML/quip-sharp/blob/main/quantize_llama/hessian_offline_llama.py
+
+import argparse
+import datetime
+import gc
+import os
+import random
+import json
+import time
+
+import numpy
+import torch
+import torch.multiprocessing as mp
+
+from transformers import AutoTokenizer
+from safetensors.torch import load_model
+
+import torch.distributed as dist
+
+from inference.model import Transformer, ModelArgs, ColumnParallelLinear, RowParallelLinear, Linear
+
+from cli_datasets import sample_rp1t
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', default=0, type=int)
+parser.add_argument('--ckpt_path', default='DeepSeek-V3-mp4', type=str)
+parser.add_argument('--save_path', default='Hessians-DeepSeek-V3', type=str)
+parser.add_argument('--sample_proc', default=72, type=int)
+parser.add_argument('--save_mem', default=False, type=bool)
+parser.add_argument('--max_samples', default=2000, type=int)
+parser.add_argument('--ctx_size', default=8192, type=int)
+parser.add_argument('--tokenizer_path', default=None, type=str)
+parser.add_argument('--dry_run', action='store_true')
+parser.add_argument('--config', default='config.json', type=str)
+parser.add_argument('--devset_size', default=256, type=int)
+
+def register_H_hook(module, device):
+    n = module.in_features
+    H = torch.zeros(n, n, dtype=torch.float64, device=device)
+    mu = torch.zeros(n, dtype=torch.float64, device=device)
+    ct = 0
+
+    def H_hook(module, x):
+        nonlocal H, mu, ct, n
+        x = x[0].reshape(-1, n).to(torch.float64)
+        mu.add_(x.sum(dim=0))
+        H.addmm_(x.T, x)
+        ct += len(x)
+
+    hook = module.register_forward_pre_hook(H_hook)
+
+    def done():
+        nonlocal H, mu, ct, hook
+        hook.remove()
+        H_cpu = H.cpu()
+        mu_cpu = mu.cpu()
+        ct_copy = ct
+        del H
+        del mu
+        clean()
+        return H_cpu, mu_cpu, ct_copy
+
+    return done
+
+
+def flat_to_sym(V, N):
+    A = torch.zeros(N, N, dtype=V.dtype, device=V.device)
+    idxs = torch.tril_indices(N, N, device=V.device)
+    A[idxs.unbind()] = V
+    A[idxs[1, :], idxs[0, :]] = V
+    return A
+
+def sym_to_flat(A):
+    N = A.shape[-1]
+    idxs = torch.tril_indices(N, N, device=A.device)
+    return A[idxs.unbind()]
+
+def hook_forward_layer(layer, device):
+    torch.set_grad_enabled(False)
+     
+    linear_layers = find_linear_layers(layer)
+    hook_list = []
+    # print(f'linear_layers: {linear_layers}')
+    
+    for name, module in linear_layers:
+        # print(f'name: {name}, module: {module}')
+        hook_list.append((name, register_H_hook(module, device)))
+    # print(f'hook_list: {hook_list}')
+    return hook_list
+
+def clean():
+    gc.collect()
+    torch.cuda.empty_cache()
+
+def find_linear_layers(module):
+    linear_layers = []
+    for name, module in module.named_modules():
+        if isinstance(module, Linear) \
+            or isinstance(module, RowParallelLinear) \
+            or isinstance(module, ColumnParallelLinear):
+            linear_layers.append((name, module))
+    return linear_layers
+
+def main(args):
+    world_size = int(os.getenv("WORLD_SIZE", "1"))
+    rank = int(os.getenv("RANK", "0"))
+    local_rank = int(os.getenv("LOCAL_RANK", "0"))
+    
+    if world_size > 1:
+        dist.init_process_group("nccl")
+        torch.cuda.set_device(local_rank)  # Set device for NCCL
+    
+    # Set CPU threads based on mode
+    torch.set_num_threads(8)
+
+    # global print
+    
+    print(f'world_size: {world_size}, rank: {rank}, local_rank: {local_rank}')
+    # if rank != 0:
+    #     print = lambda *_, **__: None
+
+    torch.set_default_dtype(torch.bfloat16)
+    torch.manual_seed(965)
+    
+    with open(args.config) as f:
+        model_args = ModelArgs(**json.load(f))
+    print(f"model_args: {model_args}")
+    
+    if args.tokenizer_path is None:
+        tokenizer = AutoTokenizer.from_pretrained(args.ckpt_path)
+    else:
+        print(f'load tokenizer from {args.tokenizer_path}')
+        tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+    
+    if rank == 0:
+        print(f"loading dataset on rank {rank}...")
+        devset = sample_rp1t(tokenizer, args.max_samples, args.ctx_size, nproc=args.sample_proc)
+        devset = devset.to(f'cuda:{rank}')
+        # print(f"Rank {rank}: Dataset loaded and moved to GPU")
+        clean()
+    else:
+        devset = torch.zeros((args.max_samples, args.ctx_size), dtype=torch.int64, device=f'cuda:{rank}')
+        #print(f"Rank {rank}: Created empty tensor for broadcast")
+    
+    if world_size > 1:
+        # print(f"Rank {rank}: Before broadcast")
+        dist.broadcast(devset, src=0)
+        dist.barrier()  # Synchronize all processes
+        # print(f"Rank {rank}: After broadcast")
+    
+    # print(f"Rank {rank}: Moving dataset to CPU")
+    devset = devset.to('cpu')
+    # print(f"Rank {rank}: Dataset moved to CPU. Shape: {devset.shape}")
+    
+    print("loading model...")
+    start_time = time.time()
+    
+    with torch.device("cpu"):  # Always load model to CPU first
+        print(f"Rank {rank}: Creating model")
+        model = Transformer(model_args)
+        if not args.dry_run:
+            print(f"Rank {rank}: Loading model weights")
+            load_model(model, os.path.join(args.ckpt_path, f"model{rank}-mp{world_size}.safetensors"))
+            print(f"Rank {rank}: Model load time: {time.time() - start_time:.2f}s")
+        else:
+            print(f"Rank {rank}: Dry run model load")
+    # print(f"Rank {rank}: Model loaded!")
+    
+    dev_emb = []
+    # print(f"Rank {rank}: Starting sample processing")
+    
+    # print(f"Rank {rank}: Moving embed layer to GPU")
+    model.embed = model.embed.to(f'cuda:{rank}')
+    # print(f"Rank {rank}: Embed layer moved to GPU")
+    
+    for idx in range(devset.shape[0]):
+        print(f'Rank {rank}: Starting to process sample {idx} / {devset.shape[0]}')
+        try:
+            _tokens = devset[idx].to(f'cuda:{rank}')
+            # print(f'Rank {rank}: Tokens moved to GPU: {_tokens.shape}')
+            _dev_emb = model.embed(_tokens)
+            # print(f'Rank {rank}: Embedding completed: {_dev_emb.shape}')
+            _dev_emb = _dev_emb.unsqueeze(0)
+            dev_emb.append(_dev_emb.to("cpu"))
+            # print(f'Rank {rank}: Sample {idx} processed successfully')
+        except Exception as e:
+            print(f'Rank {rank}: Error processing sample {idx}: {str(e)}')
+            raise
+
+    del model.embed
+    clean()
+    print(f"dev_emb: {len(dev_emb)}")
+
+    for transformer_layer_index in range(len(model.layers)):
+        print(f'processing layer {transformer_layer_index} / {len(model.layers)}')
+        transformer_layer = model.layers[transformer_layer_index]
+        
+        transformer_layer = transformer_layer.to(f'cuda:{rank}')
+        hook_list = hook_forward_layer(transformer_layer, f'cuda:{rank}')
+        if world_size > 1:
+            dist.barrier()
+         
+        # inference 
+        for idx in range(len(dev_emb)):
+            print(f'inference sample {idx} / {len(dev_emb)}')
+            _dev_emb = dev_emb[idx]
+            _start_pos = 0
+            _freqs_cis = model.freqs_cis[_start_pos:_start_pos+_dev_emb.size(1)]
+            _mask = torch.full((_dev_emb.size(1), _dev_emb.size(1)), float("-inf"), device="cpu").triu_(1)
+            _dev_emb = _dev_emb.to(f'cuda:{rank}')
+            _freqs_cis = _freqs_cis.to(f'cuda:{rank}')
+            _mask = _mask.to(f'cuda:{rank}')
+            _dev_emb = transformer_layer(_dev_emb, _start_pos, _freqs_cis, _mask)
+            torch.cuda.synchronize()
+
+            dev_emb[idx] = _dev_emb.to("cpu")
+            del _dev_emb
+            del _freqs_cis
+            del _mask
+            clean()
+        
+        if world_size > 1:
+            dist.barrier()
+        
+        # save hessian
+        for name, done in hook_list:
+            H, mu, ct = done()
+            save_path = f'{args.save_path}/H_{transformer_layer_index}_{name}.pt'
+            # Save data to disk
+            print(f'{transformer_layer_index}_{name}, H: {H.shape}, mu: {mu.shape}, ct: {ct}')
+            # torch.save({
+            #     'flatH': sym_to_flat(H.to(torch.float32)),
+            #     'mu': mu.to(torch.float32),
+            #     'n': H.shape[0],
+            #     'ct': ct
+            # }, save_path)
+            del H
+            del mu
+            clean()
+        
+        print(f'free layer')
+        transformer_layer.cpu()  # Move layer back to CPU before deletion
+        del transformer_layer
+        del hook_list
+        clean()
+        torch.cuda.empty_cache()  # Force CUDA memory cleanup
+
+    if world_size > 1:
+        dist.destroy_process_group()
+
+if __name__ == "__main__":
+    mp.set_start_method('spawn')
+    torch.set_grad_enabled(False)
+    args = parser.parse_args()
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    numpy.random.seed(args.seed)
+    os.makedirs(args.save_path, exist_ok=True)
+    main(args)
